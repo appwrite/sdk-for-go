8e46a5b1eb86784c3c7f6155a2b9b5535d34c593
loks0n <22452787+loks0n@users.noreply.github.com>
2025-04-17 20:38:03 +0100

    chore: major version bump

--- client/client.go
+++ client/client.go
@@ -74,11 +74,11 @@ type Client struct {
 func New(optionalSetters ...ClientOption) Client {
 	headers := map[string]string{
 		"X-Appwrite-Response-Format" : "1.6.0",
-		"user-agent" : fmt.Sprintf("AppwriteGoSDK/0.4.1 (%s; %s)", runtime.GOOS, runtime.GOARCH),
+		"user-agent" : fmt.Sprintf("AppwriteGoSDK/0.5.0 (%s; %s)", runtime.GOOS, runtime.GOARCH),
 		"x-sdk-name": "Go",
 		"x-sdk-platform": "server",
 		"x-sdk-language": "go",
-		"x-sdk-version": "0.4.1",
+		"x-sdk-version": "0.5.0",
 	}
 	httpClient, err := GetDefaultClient(defaultTimeout)
 	if err != nil {
