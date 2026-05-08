214226b3d31d614beec8a06133504bc19bf69e32
root <arnabchatterjee.ac.2@gmail.com>
2026-05-08 11:00:22 +0000

    feat: Go SDK update for version v3.1.0

--- client/client.go
+++ client/client.go
@@ -74,11 +74,11 @@ type Client struct {
 func New(optionalSetters ...ClientOption) Client {
 	headers := map[string]string{
 		"X-Appwrite-Response-Format" : "1.9.4",
-		"user-agent" : fmt.Sprintf("AppwriteGoSDK/v3.2.0 (%s; %s)", runtime.GOOS, runtime.GOARCH),
+		"user-agent" : fmt.Sprintf("AppwriteGoSDK/v3.1.0 (%s; %s)", runtime.GOOS, runtime.GOARCH),
 		"x-sdk-name": "Go",
 		"x-sdk-platform": "server",
 		"x-sdk-language": "go",
-		"x-sdk-version": "v3.2.0",
+		"x-sdk-version": "v3.1.0",
 	}
 	httpClient, err := GetDefaultClient(defaultTimeout)
 	if err != nil {
